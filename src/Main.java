e149954d0f2824f0c93c170b3d3bcacd6201f685
armand <g.armand2000@gmail.com>
2022-11-14 15:56:27 +0100

    creation du main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Version : 0.0");
+        System.out.println("Lancement de la Base de Donnée");
+        // code pour la bdd ici
+
+        System.out.println("Lancement du programme AL2000");
+        // code pour la class AL2000
+
+        System.out.println("Lancement de l'interface");
+        // code ihm ici
+    }
+}
