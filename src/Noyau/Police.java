638b582490619d596478d90d41210a29f3eae119
Polluxine <geoffrey.david@orange>
2022-11-17 11:23:05 +0100

    Squelette de Police Squelette de HistoLoc

--- src/Noyau/Police.java
+++ src/Noyau/Police.java
@@ -1,4 +1,20 @@
 package Noyau;
 
+import java.util.Timer;
+
+/**
+ * Police vérifie périodiquement s'il y a des locations non rendues depuis plus de
+ * 5 jours. Si oui, alors un débit sur compte bancaire est effectué.
+ * @author Geoffrey DAVID
+ * @version 0
+ */
 public class Police {
+
+    HistoLoc histo;
+    Timer chrono;
+
+    Police(HistoLoc historique, int delai){
+        histo = historique;
+        chrono = new Timer();
+    }
 }
