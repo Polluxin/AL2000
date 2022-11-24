98568d2737bc669e7f0493053a46cbca41d1b898
Polluxine <geoffrey.david@orange>
2022-11-24 09:50:36 +0100

    Implémentation des formulaires

--- src/Metier/GestionMachine/FormulaireSignalement.java
+++ src/Metier/GestionMachine/FormulaireSignalement.java
@@ -1,4 +1,30 @@
 package Metier.GestionMachine;
 
+@SuppressWarnings("unused")
 public class FormulaireSignalement {
+
+    private final String nom, prenom, adresseMail, description;
+
+    FormulaireSignalement(String nom, String prenom, String adresseMail, String description){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.description = description;
+        this.adresseMail =adresseMail;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getAdresseMail() {
+        return adresseMail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
