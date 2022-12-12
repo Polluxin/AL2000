56dd52838574eb935fe6d7f7abe394554185fcaa
Polluxine <geoffrey.david@orange>
2022-12-12 19:17:58 +0100

    Police incorrect, retrait

--- src/Tests/BD/TestHistorique.java
+++
@@ -1,8 +0,0 @@
-package Tests.BD;
-
-public class TestHistorique {
-    public static void main(String[] args) {
-        // On suppose la base créée
-        System.out.println("Historique :");
-    }
-}
